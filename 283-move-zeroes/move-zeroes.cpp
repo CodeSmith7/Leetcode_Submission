f6c35bec21bb65f16b1d939cdad41987296397b0
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2023-12-19 19:36:08 +0530

    Time: 22 ms (23.22%) | Memory: 19.6 MB (65.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -12,6 +12,10 @@ public:
         //good method 
         int i = 0;
         int j = 0;
+        
+        //0  to j-1 -> non zero
+        //j to i-1 -> zero
+        //i to end ->unknown
 
         while(i < nums.size()) {
             if (nums[i] == 0) {
