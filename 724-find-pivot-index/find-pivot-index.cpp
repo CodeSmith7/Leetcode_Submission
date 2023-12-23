df8673824c2d8170f9f075720530aa3c8a012004
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2023-12-23 21:59:07 +0530

    Time: 22 ms (34.28%) | Memory: 32 MB (22.45%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        long long  sum = 0;
-        for (auto x: nums) {
-            sum += x;
+        // long long  sum = 0;
+        // for (auto x: nums) {
+        //     sum += x;
+        // }
+        // long long lsum = 0;
+        // for (int i  = 0;i< nums.size();i++) {
+        //     sum -= nums[i];
+        //     if (lsum == sum){
+        //         return i;
+        //     } 
+        //     lsum += nums[i];
+        // }
+        // return -1;
+
+
+
+        //second method using prefixx sum 
+
+        int* pre =new int[nums.size()];
+        int sum = nums[0];
+        pre[0] = 0;
+        for (int i = 1; i < nums.size();i++) {
+            sum += nums[i];
+            pre[i] = pre[i-1] + nums[i-1];
         }
-        long long lsum = 0;
-        for (int i  = 0;i< nums.size();i++) {
-            sum -= nums[i];
-            if (lsum == sum){
+
+        for (int i = 0; i < nums.size();i++) {
+            if (pre[i] == sum - pre[i] -nums[i]) {
                 return i;
-            } 
-            lsum += nums[i];
+            }
         }
         return -1;
     }
