b6288732575b4f3fd2f215d76c93639d985bd370
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-02-10 22:47:10 +0530

    Time: 15 ms (55.41%) | Memory: 8.2 MB (12.90%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -4,8 +4,14 @@ public:
         // unsigned int maxi = pow(3,19);
         // if (n == 0) return false;
         // return (maxi % n ? false: true);        
-        if (n == 1) return true;
-        if (n <= 0 || n %3 !=0) return false;
-        return isPowerOfThree(n/3);
+        // if (n == 1) return true;
+        // if (n <= 0 || n %3 !=0) return false;
+        // return isPowerOfThree(n/3);
+        if (n==0) return false;
+        if (n == 1 ) return true;
+        if (n % 3 == 0) {
+            return isPowerOfThree(n/3);
+        }
+        else return false;
     }
 };
