04ac385ea216a1240da6f9ad222086f0e7ab1118
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-02-10 22:44:49 +0530

    Time: 12 ms (65.41%) | Memory: 8 MB (35.21%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        unsigned int maxi = pow(3,19);
-        if (n == 0) return false;
-        return (maxi % n ? false: true);        
+        // unsigned int maxi = pow(3,19);
+        // if (n == 0) return false;
+        // return (maxi % n ? false: true);        
+        if (n == 1) return true;
+        if (n <= 0 || n %3 !=0) return false;
+        return isPowerOfThree(n/3);
     }
 };
