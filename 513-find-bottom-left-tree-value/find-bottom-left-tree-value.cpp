42fbafb6570110d4ee7b243d95102ce5843d0737
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-02-28 22:27:28 +0530

    Time: 12 ms (32.78%) | Memory: 20.2 MB (63.53%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -13,20 +13,37 @@ class Solution {
 public:
     int maxDepth;
     int bottomLeft;
-    void dfs(TreeNode* root, int currDepth) {
-        if (!root) {
-            return;
-        }
-        if (currDepth > maxDepth) {
-            maxDepth = currDepth;
-            bottomLeft = root->val;
-        }
-        dfs(root->left,currDepth + 1);
-        dfs(root->right,currDepth + 1);
-    }
+
+    //Approach one (DFS)
+    // void dfs(TreeNode* root, int currDepth) {
+    //     if (!root) {
+    //         return;
+    //     }
+    //     if (currDepth > maxDepth) {
+    //         maxDepth = currDepth;
+    //         bottomLeft = root->val;
+    //     }
+    //     dfs(root->left,currDepth + 1);
+    //     dfs(root->right,currDepth + 1);
+    // }
     int findBottomLeftValue(TreeNode* root) {
-        maxDepth = -1;
-        dfs(root,0);
-        return bottomLeft;
+        // maxDepth = -1;
+        // dfs(root,0);
+        // return bottomLeft;
+        int ans;
+        queue<TreeNode*> que;
+        que.push(root);
+        while (!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            ans = node->val;
+            if (node ->right ) {
+                que.push(node->right);
+            }
+            if (node -> left ) {
+                que.push(node -> left);
+            }
+        }
+        return ans;
     }
 };
