0f9d6277e81c15fab43e3313d52724d80a0375dc
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-03-01 23:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (55.89%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -5,18 +5,31 @@ public:
 
         string res = string(n,'0');
 
-        int i = 0;
+        // int i = 0;
 
-        for (char &ch: s) {
-            if (ch == '1') {
-                if (res[n-1] == '1') {
-                    res [i] = '1';
-                    i++;
-                } else {
-                    res[n-1] = '1';
-                }
-            }
+        // for (char &ch: s) {
+        //     if (ch == '1') {
+        //         if (res[n-1] == '1') {
+        //             res [i] = '1';
+        //             i++;
+        //         } else {
+        //             res[n-1] = '1';
+        //         }
+        //     }
+        // }
+        // return res;
+
+
+        //using STL
+
+        int count_1 = count (s.begin(),s.end(),'1');
+        int j = 0;
+        while (count_1 > 1) {
+            res[j] = '1';
+            j++;
+            count_1--;
         }
+        res[n-1] = '1';
         return res;
     }
 };
