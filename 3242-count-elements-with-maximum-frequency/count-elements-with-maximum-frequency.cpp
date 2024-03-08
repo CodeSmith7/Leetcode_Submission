5f933af6cb9381a15ae08a98131556f1ec106309
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-03-08 23:39:05 +0530

    Time: 6 ms (46.85%) | Memory: 21.9 MB (17.21%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        unordered_map<int ,int > mp;
+        // unordered_map<int ,int > mp;
         
-        for (auto x: nums ) {
+        // for (auto x: nums ) {
+        //     mp[x]++;
+        // }
+        // int maxi = INT_MIN;
+        // int ans  = 0;
+        // for (auto &[a,b] : mp) {
+        //      maxi = max(maxi,b);
+        // }
+        // cout << maxi << endl;
+        // for (auto &[a,b] : mp) {
+        //     if (b == maxi) {
+        //         cout << a << endl;
+        //         ans += maxi;
+        //     }
+        // }
+        // return ans;
+
+
+        //one pass solution 
+
+        vector<int> mp(101);
+
+        int maxFreq = 0;
+        int ans = 0;
+
+        for (int &x : nums) {
             mp[x]++;
-        }
-        int maxi = INT_MIN;
-        int ans  = 0;
-        for (auto &[a,b] : mp) {
-             maxi = max(maxi,b);
-        }
-        cout << maxi << endl;
-        for (auto &[a,b] : mp) {
-            if (b == maxi) {
-                cout << a << endl;
-                ans += maxi;
+
+            int freq = mp[x];
+
+            if ( freq  >  maxFreq) {
+                maxFreq = freq;
+                ans = maxFreq;
+            }
+            else if (freq == maxFreq) {
+                ans += maxFreq;
             }
         }
+
         return ans;
+
     }
 };
