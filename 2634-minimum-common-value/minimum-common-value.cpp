c1f7817f9ae810b36033763536c7b08603f59218
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-03-09 23:51:15 +0530

    Time: 71 ms (59.37%) | Memory: 53 MB (29.59%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st(nums2.begin(),nums2.end());
+    bool binarySearch (vector<int> &nums,int target) {
+        int i = 0, j = nums.size()-1;
 
-        for (int &num : nums1) {
-            if (st.find(num) != st.end()) {
+        while (i <= j) {
+            int mid = (i + (j-i)/2);
+            if (nums[mid] == target) return true;
+            else if (nums[mid] < target) i = mid +1;
+            else j = mid -1;
+        }
+        return false;
+    }
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+    //Approach 1:
+
+        // unordered_set<int> st(nums2.begin(),nums2.end());
+
+        // for (int &num : nums1) {
+        //     if (st.find(num) != st.end()) { //unorederd set take o(1) time for finding the number 
+        //         return num;
+        //     }
+        // }
+
+        // return -1;
+
+        //approch 2 : using binarysearch
+
+        for (int &num :  nums1) {
+            if (binarySearch(nums2,num)) {
                 return num;
             }
         }
-
         return -1;
+
+
     }
 };
