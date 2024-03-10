bbe811532bb4082af4b83b175e4dd8ed81d0e4b1
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-03-10 16:50:33 +0530

    Time: 5 ms (49.43%) | Memory: 13.5 MB (12.68%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st;
-        unordered_map<int,int> mp;
-        for (auto x: nums1 ) {
-            mp[x]++;
-        }
-        for (auto x : nums2) {
-            if (mp.find(x) != mp.end()) {
-                st.insert(x);
+        // unordered_set<int> st;
+        // unordered_map<int,int> mp;
+        // for (auto x: nums1 ) {
+        //     mp[x]++;
+        // }
+        // for (auto x : nums2) {
+        //     if (mp.find(x) != mp.end()) {
+        //         st.insert(x);
+        //     }
+        // }
+        // return vector<int>(st.begin(),st.end());
+
+        //approach 1 : 
+
+        unordered_set<int> st1(nums1.begin(),nums1.end());
+
+        unordered_set<int> st2;
+
+        for (int &num : nums2) {
+            if (st1.find(num) != st1.end()) {
+                st2.insert(num);
             }
         }
-        return vector<int>(st.begin(),st.end());
+
+        vector <int> res(begin(st2),end(st2));
+        return res;
         
     }
 };
