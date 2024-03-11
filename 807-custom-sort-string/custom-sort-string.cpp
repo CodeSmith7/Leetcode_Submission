8c4f4f4967836792d2fbd98b69ef7e5a59fe8513
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-03-11 23:14:26 +0530

    Time: 4 ms (26.39%) | Memory: 8.2 MB (30.10%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        //unordered_map <char , int > mp;
-        int count[26] = {0};
-        for (char &c : s) {
-            count[c-'a']++;
-        }
-        string ans = "";
-        for (char &c : order) {
-            while (count[c-'a']--) {
-                ans += c;
-            }
+    //     Approach 1:
+    //     //unordered_map <char , int > mp;
+    //     int count[26] = {0};
+    //     for (char &c : s) {
+    //         count[c-'a']++;
+    //     }
+    //     string ans = "";
+    //     for (char &c : order) {
+    //         while (count[c-'a']--) {
+    //             ans += c;
+    //         }
+    //     }
+
+    //    for (char &ch : s ) {
+    //        if (count [ch-'a'] > 0) {
+    //            ans += ch;
+    //        }
+    //    }
+
+    //     return ans;
+
+        
+        //Approach 2: 
+
+        vector <int> index(26,-1);
+        int i = 0;
+
+        for (char &ch : order) {
+            index[ch-'a'] = i;
+            i++;
         }
 
-       for (char &ch : s ) {
-           if (count [ch-'a'] > 0) {
-               ans += ch;
-           }
-       }
+        //lambda Expression
+        auto myComprator = [&index] (char &ch1, char &ch2) {
+            return index[ch1-'a'] < index[ch2-'a'];
+        };
 
-        return ans;
+        sort (begin(s),end(s),myComprator); 
+        return s;
     }
 };
