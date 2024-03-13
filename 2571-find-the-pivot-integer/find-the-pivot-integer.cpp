70980c3422e6e6f9f272b39ca108317f0708f5da
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-03-13 20:12:40 +0530

    Time: 3 ms (51.39%) | Memory: 7.3 MB (15.68%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int i = 1;
-        int j = n;
+        //Approach : two pointer
+        // int i = 1;
+        // int j = n;
         
-        int leftSum = 1;
-        int rightSum = n;
+        // int leftSum = 1;
+        // int rightSum = n;
 
-        while ( i < j) {
-            if (leftSum < rightSum ) {
-                i++;
-                leftSum += i;
-            }
-            else  {
-                j--;
-                rightSum += j;
-            }
-        }
+        // while ( i < j) {
+        //     if (leftSum < rightSum ) {
+        //         i++;
+        //         leftSum += i;
+        //     }
+        //     else  {
+        //         j--;
+        //         rightSum += j;
+        //     }
+        // }
 
-        return leftSum == rightSum ? i : -1;
+        // return leftSum == rightSum ? i : -1;
+
+
+        //Approach : mathematic O(1)
+
+        int totalSum = n * (n+1)/2;
+
+        int pivot = sqrt(totalSum);
+
+        return (pivot * pivot) == totalSum ? pivot : -1;
         
     }
 };
