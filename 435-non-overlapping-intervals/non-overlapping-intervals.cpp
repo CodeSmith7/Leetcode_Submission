334842594febb70652f9d3a6da0d1b7c02ef8616
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-03-19 00:07:02 +0530

    Time: 267 ms (33.17%) | Memory: 93.4 MB (47.73%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,38 +1,72 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-         sort (begin(intervals),end(intervals));
+        //Approach 1 :
+        
+        //  sort (begin(intervals),end(intervals));
 
-        int i = 0,j = 1;
-        int count = 0;
+        // int i = 0,j = 1;
+        // int count = 0;
     
-        while ( j < intervals.size()) {
+        // while ( j < intervals.size()) {
            
-            vector<int> currInterval = intervals[i];
-            vector<int> nextInterval = intervals[j];
+        //     vector<int> currInterval = intervals[i];
+        //     vector<int> nextInterval = intervals[j];
 
-            int cs = currInterval[0];
-            int ce = currInterval[1];
-            int ns = nextInterval[0];
-            int ne = nextInterval[1];
+        //     int cs = currInterval[0];
+        //     int ce = currInterval[1];
+        //     int ns = nextInterval[0];
+        //     int ne = nextInterval[1];
 
 
            
-            if (ce <= ns) {//no overapping 
-                i = j;
-                j++;
-            }
-            else if  (ce <= ne) { // ovelapping
-                j++;
+        //     if (ce <= ns) {//no overapping 
+        //         i = j;
+        //         j++;
+        //     }
+        //     else if  (ce <= ne) { // ovelapping
+        //         j++;
+        //         count++;
+        //     }
+        //     else if (ce > ne ) {
+        //         i = j;
+        //         count++;
+        //         j++;
+        //     }
+        // }
+
+        // return count;
+
+        //Approach 2 :  
+
+        sort (begin(intervals),end(intervals));
+
+        int n = intervals.size();
+
+        int count = 0;
+        int i = 1;
+        vector<int> lastInterval = intervals[0];
+
+        while (i < n ) {
+            int currEnd = intervals[i][1];
+            int currStart = intervals[i][0];
+
+            int lastEnd = lastInterval[1];
+
+            if ( currStart >= lastEnd) {
+                //safe 
+                lastInterval = intervals[i];
+                
+            } else if (currEnd >= lastEnd ) {
+                
+                count++;
+            } else if (currEnd < lastEnd) {
+                lastInterval = intervals[i];
                 count++;
             }
-            else if (ce > ne ) {
-                i = j;
-                count++;
-                j++;
-            }
+            i++;
         }
-
         return count;
+
     }
 };
