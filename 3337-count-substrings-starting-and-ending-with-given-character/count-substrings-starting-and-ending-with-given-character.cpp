fe2ef7da1e8b7bdad66d63f6dabd4de1c92ff764
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-03-21 00:12:23 +0530

    Time: 10 ms (77.46%) | Memory: 11.2 MB (38.38%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
+    long long fact (long long a) {
+        if (a == 0) return 1;
+        return a * fact(a-1);
+    }
     long long countSubstrings(string s, char c) {
-        long long count = 0;
-        long long ans = 0;
+       //Approach 1  :
 
-        for (char &ch : s) {
-            if (ch == c) {
-                ans += 1;
-                ans += count;
-                count += 1;
-            }
+        // long long count = 0;
+        // long long ans = 0;
+
+        // for (char &ch : s) {
+        //     if (ch == c) {
+        //         ans += 1;
+        //         ans += count;
+        //         count += 1;
+        //     }
+        // }
+        // return ans;
+
+        // Approach 2 : using maths 
+        long long count = 0;
+
+        for (char &ch : s ) {
+            if (ch == c) count++;
         }
-        return ans;
+
+        return count *(count-1)/2 + count;
+
+
+
     }
 };
