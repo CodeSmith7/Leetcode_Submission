fd4eabbab55d72d4bdde72662899323733fca721
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-04-09 23:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (67.56%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int step = 0;
-        queue<int> q;
+       //Approach 1-> simulation of the data structure
+        // int step = 0;
+        // queue<int> q;
 
-        for (int i = 0;i < tickets.size();i++) {
-            q.push(i);
-        }
+        // for (int i = 0;i < tickets.size();i++) {
+        //     q.push(i);
+        // }
 
-        while (tickets[k] != 0) {
-            int ele =  q.front();
-            q.pop();
-            tickets[ele]--;
-            if (tickets[ele] != 0) {
-                q.push(ele);
+        // while (tickets[k] != 0) {
+        //     int ele =  q.front();
+        //     q.pop();
+        //     tickets[ele]--;
+        //     if (tickets[ele] != 0) {
+        //         q.push(ele);
+        //     }
+        //     step++;
+        // }
+
+        // return step;
+
+        //One pass solution
+
+        int time = 0;
+
+        int n = tickets.size();
+
+        for (int i = 0;i < n;i++) {
+            if (i <= k) {
+                time += min(tickets[i],tickets[k]);
+            } else {
+                time += min (tickets[k] -1, tickets[i] );
             }
-            step++;
         }
-
-        return step;
+        return time;
     }
 };
