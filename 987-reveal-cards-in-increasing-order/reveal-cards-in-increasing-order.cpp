fe1f977f930bcb612df5996bd79751b4d75cee55
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-04-10 23:37:03 +0530

    Time: 7 ms (18.53%) | Memory: 11 MB (34.66%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -1,28 +1,58 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+      //Approach 1 : simulation
+        // int n = deck.size();
+        // vector <int> result (n,0);
+
+        // bool skip = false;
+
+        // int i = 0;//deck
+        // int j = 0;//result
+
+        // sort(begin(deck),end(deck));
+
+        // while (i < n) {
+
+        //     if (result[j] == 0) {//empt hai
+
+        //         if (skip == false) {
+        //             result[j] = deck[i];
+        //             i++;
+        //         }
+        //         skip = !skip;//alternate
+        //     }
+
+        //     j = (j+1)%n;
+        // }
+        // return result;
+
+        //Approach 2 : using proper simulation use queue
+
         int n = deck.size();
-        vector <int> result (n,0);
 
-        bool skip = false;
+        queue<int> que;
+        vector <int> result(n);
+       
 
-        int i = 0;//deck
-        int j = 0;//result
-
+        for (int i = 0; i < n;i++) {
+            que.push(i);
+        }
+        
         sort(begin(deck),end(deck));
+        int i = 0;
 
-        while (i < n) {
+        while ( i < n) {
+            int idx = que.front();
+            que.pop();
+            result[idx] = deck[i];
 
-            if (result[j] == 0) {//empt hai
-
-                if (skip == false) {
-                    result[j] = deck[i];
-                    i++;
-                }
-                skip = !skip;//alternate
+            if(!que.empty()) {
+                que.push(que.front());
+                que.pop();
             }
-
-            j = (j+1)%n;
+            i++;
+            
         }
         return result;
     }
