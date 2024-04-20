753da234a59b8b5fbf66af5f8265fb0f9cd5434e
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-04-20 23:30:37 +0530

    Time: 108 ms (6.13%) | Memory: 108.3 MB (5.31%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -3,7 +3,41 @@ public:
     int m;
     int n;
     int peri;
-    
+
+    vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
+
+    int bfs(vector<vector<int>>& grid,int i,int j) {
+        int perimeter = 0;
+
+        queue<pair<int,int>> que;
+        que.push({i,j});
+        grid[i][j] = -1;
+
+        while (!que.empty()) {
+            
+            auto it = que.front();
+            que.pop();
+
+            for (auto &direction: directions) {
+                int i_ = it.first + direction[0];
+                int j_ = it.second + direction[1];
+
+                if ( i_ < 0 || i_ >= m || j_ < 0 || j_ >= n || grid[i_][j_] == 0) {
+                    perimeter++;
+                }
+                else if ( grid[i_][j_] == -1) {
+                    continue;
+                } else {
+                    que.push({i_,j_});
+                    grid[i_][j_] = -1;
+                }
+            }
+
+            
+        }
+
+        return perimeter;
+    }
     void dfs(vector<vector<int>>& grid, int i, int j) {
         
         if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
@@ -25,6 +59,8 @@ public:
     }
     
     int islandPerimeter(vector<vector<int>>& grid) {
+        //Approach 1 : BFS
+
         m    = grid.size();
         n    = grid[0].size();
         peri = 0;
@@ -33,8 +69,10 @@ public:
             for(int j = 0; j < n; j++) {
                 
                 if(grid[i][j] == 1) {
-                    dfs(grid, i, j);
-                    return peri;
+                    // dfs(grid, i, j);
+                    // return peri;
+
+                    return bfs(grid,i,j);
                 }
                 
             }
