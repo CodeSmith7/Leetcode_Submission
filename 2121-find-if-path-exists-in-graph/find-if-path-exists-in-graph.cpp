c21f531563b7770409a32636e7d2654e6a213552
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-04-21 22:09:38 +0530

    Time: 2732 ms (5.01%) | Memory: 256.4 MB (12.93%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool check (unordered_map<int,vector<int>> &adj,int s,int d, vector<bool> &visited) {
+        if (s == d ) return true;
+
+        if (visited[s]) return false;
+
+        visited[s] = true;
+        
+        //explore 
+        for(auto &node : adj[s]) {
+            if (check(adj,node,d,visited) == true) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        //adjacency list 
+        unordered_map<int, vector<int>> adj;
+
+        for (auto &edge: edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            //bidirectional graph 
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<bool>  visited(n,false);
+
+        return check(adj,source,destination,visited);
+
+    }
+};
