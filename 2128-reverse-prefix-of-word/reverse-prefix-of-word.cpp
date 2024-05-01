f854d01f5a1a6aab104a2103468fef963053da9a
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-05-01 23:56:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (65.62%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-      
-        int idx = -1;
-        // int j = 0;
-      
-        // while (j < word.size()) {
-        //     if (word[j] == ch) {
-        //         idx = j;
-        //         break;
-        //     } 
-        //     j++;
-            
-        // }
-        idx = word.find(ch);
-        if (idx == string::npos ) {
-            return word;
-        }
+      // APPROACH 1: using brute force
 
-        int i = 0;
-        while (i < idx) {
-            swap(word[i++],word[idx--]);
-        }
+        // int idx = -1;
+        // // int j = 0;
+      
+        // // while (j < word.size()) {
+        // //     if (word[j] == ch) {
+        // //         idx = j;
+        // //         break;
+        // //     } 
+        // //     j++;
+            
+        // // }
+
+        // idx = word.find(ch);
+        // if (idx == string::npos ) {
+        //     return word;
+        // }
+
+        // int i = 0;
+        // while (i < idx) {
+        //     swap(word[i++],word[idx--]);
+        // }
+
+        // return word;
+
+        //Approach 2 : using the library function only 
+
+        //reverse(iterator,iterator); reverse[iterator1, iterator2)
+
+        int j = word.find(ch);
+
+        reverse(word.begin(), word.begin() + j + 1);
 
         return word;
 
