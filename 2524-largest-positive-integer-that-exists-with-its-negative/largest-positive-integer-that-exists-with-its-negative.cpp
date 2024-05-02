d29f27ef89588d871a8bb3e3f11debe68160db8f
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-05-03 01:28:41 +0530

    Time: 19 ms (58.42%) | Memory: 22.8 MB (79.58%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        int arr[2001] = {0};
-        int ans = -1;
-        
-        int result = -1;
+       
+       //Approach 2: using sort and two pointer 
 
-        for (int i = 0;i < nums.size();i++) {
-            int counter = -nums[i] + 1000;
-            if (arr[counter] == 1) {
-               result = abs(nums[i]);
-            } else if (arr[counter] == 0) {
-                arr[nums[i] + 1000] = 1;
+       sort(begin(nums),end(nums));
+       int ans = -1;
+
+       int i = 0;
+       int j = nums.size()-1;
+
+        while (i < j) {
+         
+            if (-nums[i] == nums[j]) {
+                return nums[j];
+            } 
+            if (-nums[i] < nums[j]) {
+                 j--;
             }
-            ans = max(ans,result);
+            else {
+                i++;
+            }
         }
         return ans;
+       
+       //Approach 4 : using nums[i]
+        // int arr[2001] = {0};
+        // int ans = -1;
+        
+        // int result = -1;
+
+        // for (int i = 0;i < nums.size();i++) {
+        //     int counter = -nums[i] + 1000;
+        //     if (arr[counter] == 1) {
+        //        result = abs(nums[i]);
+        //     } else if (arr[counter] == 0) {
+        //         arr[nums[i] + 1000] = 1;
+        //     }
+        //     ans = max(ans,result);
+        // }
+        // return ans;
     }
 };
