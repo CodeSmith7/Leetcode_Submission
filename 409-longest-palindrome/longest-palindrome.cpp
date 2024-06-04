d8a6d83abd7dd16c64455f927874058a95f52d3c
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-05 00:22:02 +0530

    Time: 6 ms (16.33%) | Memory: 8.9 MB (48.94%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -3,19 +3,41 @@ public:
     int longestPalindrome(string s) {
         //Approach 1 : using SET
         
-        int n = s.length();
-        unordered_set<int> st;
-        int result = 0;
+        // int n = s.length();
+        // unordered_set<int> st;
+        // int result = 0;
 
+        // for (char &ch : s) {
+        //     if (st.count(ch) ) {
+        //         result += 2;
+        //         st.erase(ch);
+        //     } else {
+        //         st.insert(ch);
+        //     }
+        // }
+        // if (!st.empty()) {
+        //     result++;
+        // }
+        // return result;
+
+        //Approach 2: using unordered map 
+        int n = s.size();
+
+        unordered_map<char,int> mp;
         for (char &ch : s) {
-            if (st.count(ch) ) {
-                result += 2;
-                st.erase(ch);
+            mp[ch]++;
+        }
+        bool isOddPresent= false;
+        int result = 0;
+        for (auto& it: mp) {
+            if (it.second %2 == 0) {
+               result += it.second; 
             } else {
-                st.insert(ch);
+                result += it.second -1;
+                isOddPresent = true;
             }
         }
-        if (!st.empty()) {
+        if (isOddPresent) {
             result++;
         }
         return result;
