4356ef65e7b2d11a4c40a52c21787f7163ee0669
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-08 23:01:13 +0530

    Time: 71 ms (83.57%) | Memory: 76.7 MB (49.65%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,32 +1,107 @@
+//   Approoach 1 : using map 
+// class Solution {
+// public:
+//     string findRoot(string& word, unordered_set<string>& st) {
+
+//         //try all length substring starting from 0th index
+//         for(int l = 1; l <= word.length(); l++) {
+//             string root = word.substr(0, l);
+//             if(st.count(root)) {
+//                 return root;
+//             }
+//         }
+
+//         return word;
+
+//     }
+
+//     string replaceWords(vector<string>& dictionary, string sentence) {
+//         unordered_set<string> st(dictionary.begin(), dictionary.end());
+
+//         stringstream ss(sentence);
+//         string word;
+//         string result;
+
+//         while(getline(ss, word, ' ')) {
+//             result += findRoot(word, st) + " ";
+//         }
+
+//         //result.pop_back();
+//         return result.substr(0,result.size()-1);
+
+//     }
+// };
+
+
+//Approach 2 : uaing Trie
+
 class Solution {
 public:
-    string findRoot(string& word, unordered_set<string>& st) {
+    struct trieNode { 
+        trieNode *children[26]; 
+        bool isEndOfWord; 
+    };
+    
+    trieNode* getNode() {
+        trieNode* newNode = new trieNode();
+        newNode->isEndOfWord = false;
+        
+        for (int i = 0; i < 26; i++) {
+			newNode->children[i] = NULL;
+		}
+        
+        return newNode;
+    }
 
-        //try all length substring starting from 0th index
-        for(int l = 1; l <= word.length(); l++) {
-            string root = word.substr(0, l);
-            if(st.count(root)) {
-                return root;
+            
+    trieNode* root;
+
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        trieNode *crawler = root; 
+        
+        for (int i = 0; i < word.length(); i++) { 
+            int index = word[i] - 'a'; 
+            if (!crawler->children[index]) 
+                crawler->children[index] = getNode();
+            
+            crawler = crawler->children[index]; 
+        }
+        
+        crawler->isEndOfWord = true; 
+    }
+    
+    /** Returns if the word is in the trie. */
+    string search(string word) {
+        trieNode *crawler = root; 
+        
+        for (int i = 0; i < word.length(); i++) { 
+            int index = word[i] - 'a'; 
+            if (!crawler->children[index]) 
+                return word; 
+            crawler = crawler->children[index];
+            if(crawler->isEndOfWord) {
+                return word.substr(0, i+1);
             }
         }
-
         return word;
-
     }
 
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_set<string> st(dictionary.begin(), dictionary.end());
 
         stringstream ss(sentence);
         string word;
         string result;
-
-        while(getline(ss, word, ' ')) {
-            result += findRoot(word, st) + " ";
+        root = getNode();
+        for (string word : dictionary) {
+            insert(word);
         }
 
-        //result.pop_back();
-        return result.substr(0,result.size()-1);
+        while(getline(ss, word, ' ')) {
+            result += search(word) + " ";
+        }
 
+        result.pop_back();
+        return result;
     }
 };
