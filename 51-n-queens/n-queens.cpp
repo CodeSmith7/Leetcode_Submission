1bb16933ac16ec0367eaefe599114d282405b3ac
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-13 00:01:22 +0530

    Time: 3 ms (79.71%) | Memory: 9.7 MB (58.09%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
+
+    //Approach 1 : traditional n queen 
+    //Approach 2 : optimiszing in isVALID by using unordredset performing operationn in O(1)
+
     vector<vector<string>> result;
+
+    unordered_set<int> cols;
+    unordered_set<int> diagonal;
+    unordered_set<int> antiDiagonal;
 
     bool isValid (vector<string>& board,int row ,int col) {
         int n = board.size();
@@ -45,12 +53,57 @@ public:
             }
         }
     }
+    void solve2 (vector<string>& board,int row) {
+        int n = board.size();
+        
+        if (row >= n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n;col++) {
+            
+            int diagonalConst = row + col;
+            int antiDiagonalConst = row - col;
+
+            if (cols.find(col) !=  cols.end() || diagonal.find(diagonalConst) != diagonal.end() || antiDiagonal.find(antiDiagonalConst) != antiDiagonal.end() ) {
+                continue;
+            }
+
+            cols.insert(col);
+            diagonal.insert(row + col);
+            antiDiagonal.insert(row - col);
+            board[row][col] = 'Q';
+
+            solve2 (board, row + 1);
+
+            cols.erase(col);
+            diagonal.erase(row + col);
+            antiDiagonal.erase(row - col);
+            board[row][col] = '.';
+
+
+
+        }
+    }
     vector<vector<string>> solveNQueens(int n) {
         
+        // vector<string> board(n,string(n,'.'));
+
+        // solve (board,0);
+
+        // return result;
+
+
+        //approach 2:
+
+        
         vector<string> board(n,string(n,'.'));
+        
 
         solve (board,0);
 
         return result;
     }
+
 };
