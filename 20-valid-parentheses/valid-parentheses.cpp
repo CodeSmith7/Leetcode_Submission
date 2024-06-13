a3dbb51911aeac0be0dad205cffaa1ee83ead90c
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-13 12:30:21 +0530

    Time: 3 ms (41.90%) | Memory: 8 MB (15.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,39 +1,59 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int> st;
-        for (char &ch : s) {
-            if (st.empty() || ch =='(' || ch == '{' || ch == '[') {
-                st.push (ch);
-                continue;
-            }
-            if (ch == ')') {
-                if (st.top() == '(') {
-                    st.pop();
-                } else {
-                    return false;
-                }
+       //Approach 1 : using stack
+        // stack<int> st;
+        // for (char &ch : s) {
+        //     if (st.empty() || ch =='(' || ch == '{' || ch == '[') {
+        //         st.push (ch);
+        //         continue;
+        //     }
+        //     if (ch == ')') {
+        //         if (st.top() == '(') {
+        //             st.pop();
+        //         } else {
+        //             return false;
+        //         }
 
-            }
-            if (ch == '}') {
-                if (st.top() == '{') {
-                    st.pop();
-                } else {
-                    return false;
-                }
+        //     }
+        //     if (ch == '}') {
+        //         if (st.top() == '{') {
+        //             st.pop();
+        //         } else {
+        //             return false;
+        //         }
 
-            }
-            if (ch == ']') {
-                if (st.top() == '[') {
-                    st.pop();
-                } else {
-                    return false;
-                }
+        //     }
+        //     if (ch == ']') {
+        //         if (st.top() == '[') {
+        //             st.pop();
+        //         } else {
+        //             return false;
+        //         }
 
-            }
+        //     }
 
             
+        // }
+        // return st.empty();
+
+        //Approach 2: tricky approach
+        stack<char> st;
+        
+        for(char ch:s) {
+            if (ch == '(')
+			    st.push(')');
+		    else if (ch == '{')
+			    st.push('}');
+            else if (ch == '[')
+                st.push(']');
+            else if (st.empty() || st.top() != ch)
+                return false;
+            else {
+                st.pop();
+            }
         }
+        
         return st.empty();
     }
 };
