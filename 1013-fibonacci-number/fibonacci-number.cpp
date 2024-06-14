804de70b158fb79b25d2f20447fcb6e41611cad6
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-14 11:26:13 +0530

    Time: 2 ms (80.49%) | Memory: 7 MB (94.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -13,13 +13,48 @@ public:
         return dp[n] = solve (n-1, dp) + solve (n-2,dp);
     }
     int fib(int n) {
-        if (n <= 1) {
+       //Top down Memoization
+
+        // if (n <= 1) {
+        //     return n;
+        // }
+        // vector <int>  dp (n+1, -1);
+        
+        // solve(n,dp); 
+        
+        // return dp[n];
+
+        //Bottom UP Tabulation
+
+        // if (n <= 1) {
+        //     return n;
+        // }
+
+        // vector<int> dp(n+1,-1);
+
+        // //definiing the the sate dp[i] = i'th fibnacci number  
+        // dp[0] = 0;
+        // dp[1] = 1;
+
+        // for (int i = 2; i <= n;i++) {
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        // using constant space 
+        if ( n <= 1) {
             return n;
         }
-        vector <int>  dp (n+1, -1);
-        
-        solve(n,dp); 
-        
-        return dp[n];
+        int c = 0;
+        int a = 0, b = 1;
+
+        for (int i = 2; i <= n; i++) {
+            c = a+b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+
     }
 };
