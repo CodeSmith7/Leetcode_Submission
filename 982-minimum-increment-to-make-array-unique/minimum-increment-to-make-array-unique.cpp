3d4869229020a380494145070eb5a67cb74528b6
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-15 10:09:57 +0530

    Time: 84 ms (96.95%) | Memory: 74 MB (13.36%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,17 +1,45 @@
 class Solution {
 public:
-    int minIncrementForUnique(vector<int>& nums) {
-        sort(begin(nums), end(nums));
+    // int minIncrementForUnique(vector<int>& nums) {
+    //     sort(begin(nums), end(nums));
+
+    //     int moves = 0;
+
+    //     for(int i = 1; i < nums.size(); i++) {
+    //         if(nums[i] <= nums[i-1]) {
+    //             moves += (nums[i-1] - nums[i] + 1);
+    //             nums[i] = nums[i-1]+1;
+    //         }
+    //     }
+
+    //     return moves;
+    // }
+
+    //Approch 2 
+     int minIncrementForUnique(vector<int>& nums) {
+        int n = nums.size();
+        int maxElement = *max_element(begin(nums),end(nums));
+
+        vector<int> count(maxElement + n,0);
+
+        for (int &num : nums) {
+            count[num]++;
+        }
 
         int moves = 0;
 
-        for(int i = 1; i < nums.size(); i++) {
-            if(nums[i] <= nums[i-1]) {
-                moves += (nums[i-1] - nums[i] + 1);
-                nums[i] = nums[i-1]+1;
+        for (int i = 0;i < maxElement + n;i++) {
+            if (count[i] <= 1) {
+                continue;
             }
+
+            int extra = count[i] -1;
+            count[i+1] += extra;
+            count[i] = 1;
+            moves += extra;
         }
 
         return moves;
     }
 };
+
