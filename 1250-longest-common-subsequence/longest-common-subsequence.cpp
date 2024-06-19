62d319629ecffc8620e0533a7d8093a8e03027f0
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-19 10:39:46 +0530

    Time: 32 ms (34.40%) | Memory: 12 MB (80.25%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,26 +1,3 @@
-// class Solution {
-// public:
-//     int t[1001][1001];
-
-//     int solve (string text1, string text2,int i ,int j) {
-//         if (i >= text1.size() || j >= text2.size()) {
-//             return 0;
-//         }
-//         if (t[i][j] != -1) {
-//             return t[i][j];
-//         }
-
-//         if (text1[i] == text2[j]) {
-//             return t[i][j] = 1 + solve (text1,text2,i+1,j+1);
-//         } else {
-//             return t[i][j] = max(solve (text1,text2,i+1,j), solve (text1,text2,i,j+1));
-//         }
-//     }
-//     int longestCommonSubsequence(string text1, string text2) {
-//         memset(t,-1,sizeof(t));
-//         return solve (text1,text2,0,0);
-//     }
-// };
 
 class Solution {
 public:
