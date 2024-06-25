325732111988baeb1bc8e0c59e179fbe79985f41
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-25 16:45:55 +0530

    Time: 2 ms (49.63%) | Memory: 10.1 MB (77.24%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -11,51 +11,28 @@
  */
 class Solution {
 public:
-    vector<int> ans;
+
+    int sum = 0;
     
     void solve (TreeNode* root) {
         if (root == NULL) {
             return;
         }
-        solve (root -> left);
-        ans.push_back(root->val);
         solve (root -> right);
 
-    }
-    void greaterSumTree (TreeNode* root,int &i) {
-        if (root == NULL ) {
-            return;
-        }
-        
-        greaterSumTree (root -> left,i);
-        
-        root -> val = ans[i++];
+        //modify the   root
+        sum += root->val;
+        root->val = sum;
 
-        greaterSumTree (root -> right, i);
+        solve (root -> left);
 
     }
+
+   
     TreeNode* bstToGst(TreeNode* root) {
         
         solve(root);
         
-        //making the modiy array of the sum 
-        int i = ans.size() -1;
-        int sum = 0;
-
-        while (i >= 0) {
-            ans[i] += sum;
-            sum = ans[i];
-            i--;
-        }
-        cout << " modified array is : ->  " << endl;
-        for (int &ele : ans) {
-            cout << ele << " ";
-        }
-
-        int k = 0;
-
-        greaterSumTree(root,k
-        );
 
         return root;
     }
