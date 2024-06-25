0e6432f86924acb36d770ba92e20c6c79f7cbd49
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-25 15:52:06 +0530

    Time: 101 ms (23.45%) | Memory: 112.3 MB (38.68%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,15 +1,52 @@
+//Approach 1 : (Using auxiliary array to mark Flipped indices)
+// class Solution {
+// public:
+//     int minKBitFlips(vector<int>& nums, int k) {
+//         int n = nums.size();
+
+//         int flips = 0;
+//         vector<bool> isFlipped(n, false);
+//         int flipCountFromPastForCurri = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             if(i >= k && isFlipped[i-k] == true){
+//                 flipCountFromPastForCurri--;                     
+//             }
+            
+//             if(flipCountFromPastForCurri % 2 == nums[i]) {
+//                 if(i+k > n){
+//                     return -1;
+//                 }
+//                 flipCountFromPastForCurri++;
+//                 isFlipped[i] = true;
+//                 flips++;
+//             }
+//         }
+
+//         return flips;
+
+//     }
+// };
+
+//Approach 2 make the change in given arrauy only 
+
+//Approach 3 using deque
+
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         int n = nums.size();
+
+        deque<int> dq;
 
         int flips = 0;
         vector<bool> isFlipped(n, false);
         int flipCountFromPastForCurri = 0;
 
         for(int i = 0; i < n; i++) {
-            if(i >= k && isFlipped[i-k] == true){
-                flipCountFromPastForCurri--;                     
+            if(i >= k ){
+                flipCountFromPastForCurri -= dq.front();
+                dq.pop_front();                     
             }
             
             if(flipCountFromPastForCurri % 2 == nums[i]) {
@@ -17,8 +54,10 @@ public:
                     return -1;
                 }
                 flipCountFromPastForCurri++;
-                isFlipped[i] = true;
+                dq.push_back(1);
                 flips++;
+            } else {
+                dq.push_back(0);
             }
         }
 
