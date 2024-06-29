16cdb4bcda562db36b5db07cbc1bbf5b8199093f
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-06-29 19:12:52 +0530

    Time: 198 ms (44.68%) | Memory: 74.6 MB (63.34%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,18 +1,58 @@
+//Approch 1 : 
+// class Solution {
+// public:
+//     void DFS(int ancestor,unordered_map<int, vector<int>>& adj, int currNode, vector<vector<int>>& result) {
+//         for (int &ngbr : adj[currNode]) {
+//             if (result[ngbr].empty() || result[ngbr].back() != ancestor) {
+//                 result[ngbr].push_back(ancestor);
+//                 DFS(ancestor, adj, ngbr, result);
+//             }
+//         }
+//     }
+    
+
+
+//     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+//         vector<vector<int>> result(n);
+//         unordered_map<int, vector<int> > adj;
+
+//         for (auto &vec: edges) {
+//             int u = vec[0];
+//             int v = vec[1];
+
+//             //unidirectional graph 
+//             adj[u].push_back(v);
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             int ancestor = i;
+//             DFS(ancestor, adj, i, result);
+//         }
+
+//         return result;
+        
+//     }
+// };
+
+
+//Approach 2 : by reversing the edges 
 class Solution {
 public:
-    void DFS(int ancestor,unordered_map<int, vector<int>>& adj, int currNode, vector<vector<int>>& result) {
-        for (int &ngbr : adj[currNode]) {
-            if (result[ngbr].empty() || result[ngbr].back() != ancestor) {
-                result[ngbr].push_back(ancestor);
-                DFS(ancestor, adj, ngbr, result);
+    void DFS(int u,unordered_map<int, vector<int>>& adj, vector<bool>& visited) { 
+        visited[u] = true;
+
+        for (int &v : adj[u]) {
+            if (visited[v] == false) {
+                DFS(v,adj,visited);
             }
         }
+        
     }
     
 
 
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> result(n);
+        vector<vector<int>> result;
         unordered_map<int, vector<int> > adj;
 
         for (auto &vec: edges) {
@@ -20,12 +60,23 @@ public:
             int v = vec[1];
 
             //unidirectional graph 
-            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
-        for (int i = 0; i < n; i++) {
-            int ancestor = i;
-            DFS(ancestor, adj, i, result);
+        for (int u = 0; u < n; u++) {
+            vector<int> ancestor;
+            vector<bool> visited(n,false);
+
+            DFS(u,adj,visited);
+
+            for (int i = 0; i < n; i++) {
+                if (i == u) continue;
+
+                if (visited[i] == true) {
+                    ancestor.push_back(i);
+                }
+            }
+            result.push_back(ancestor);
         }
 
         return result;
