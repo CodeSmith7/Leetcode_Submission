a0e5b107e5c67962cd0e3ac762084d2bc4cbd001
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-07-01 08:07:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (45.76%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -1,16 +1,47 @@
+//Approach 1: using sinple for loop chekcing each element 
+// class Solution {
+// public:
+//     bool threeConsecutiveOdds(vector<int>& arr) {
+
+//         int n  = arr.size();
+        
+//         for (int i = 0; i < n-2;i++) {
+//             if (arr[i] &1 && arr[i+1] & 1 && arr[i+2] & 1) {
+//                 return true;
+//             }
+//         }
+
+//         return false;;
+        
+//     }
+// };
+
+
+//Approach 2 : using counter to track of consequtive odd number 
+
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
 
         int n  = arr.size();
         
-        for (int i = 0; i < n-2;i++) {
-            if (arr[i] &1 && arr[i+1] & 1 && arr[i+2] & 1) {
+        int count = 0;
+        for (int i = 0;i < n;i++) {
+
+            if (arr[i] & 1) {
+                count++;  //count it 
+            } else {
+                count = 0; //Reset it
+            }
+
+            if (count == 3) {
                 return true;
             }
+
         }
 
-        return false;;
+        return false;
         
     }
 };
+
