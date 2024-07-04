93832aed3792cdf259fb18eca867aa1c036fa254
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-07-04 13:20:19 +0530

    Time: 413 ms (81.73%) | Memory: 259.6 MB (68.34%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -8,29 +8,54 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ //Approach 1 : using simple traversal
+// class Solution {
+// public:
+//     ListNode* mergeNodes(ListNode* head) {
+
+//         int sum = 0;
+//         ListNode* p1 = head->next;
+//         ListNode* p2 = p1;
+
+//         while ( p2 != NULL) {
+            
+//             sum = 0;
+            
+//             while ( p2 != NULL && p2-> val != 0) {
+//                 sum += p2->val;
+//                 p2 = p2 -> next;
+//             }
+
+//             p1 -> val = sum;
+//             p2 = p2->next;
+//             p1->next = p2;
+//             p1 = p1->next;
+            
+//         }
+//         return head->next;
+//     }
+// };
+
+
+//Approach 2 : usinf recursion 
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
 
-        int sum = 0;
-        ListNode* p1 = head->next;
-        ListNode* p2 = p1;
-
-        while ( p2 != NULL) {
-            
-            sum = 0;
-            
-            while ( p2 != NULL && p2-> val != 0) {
-                sum += p2->val;
-                p2 = p2 -> next;
-            }
-
-            p1 -> val = sum;
-            p2 = p2->next;
-            p1->next = p2;
-            p1 = p1->next;
-            
+        head = head -> next;
+        if (head == NULL) {
+            return head;
         }
-        return head->next;
+
+        ListNode* temp = head;
+        int sum = 0;
+
+        while (temp != NULL && temp -> val != 0) {
+            sum += temp ->val;
+            temp = temp->next;
+        }
+        head -> val = sum;
+        head -> next = mergeNodes(temp);
+        return head;
     }
 };
