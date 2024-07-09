192329499f4fea8e929092d1114d4f5e574f16fa
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-07-09 06:09:47 +0530

    Time: 15 ms (52.04%) | Memory: 20.4 MB (31.16%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,15 +1,27 @@
+//Approach 1: using math
+// class Solution {
+// public:
+
+//     int missingNumber(vector<int>& nums) {
+
+//         int ans =  (nums.size() * (nums.size() +1))/2 ;
+//         for (auto x: nums)
+//            ans -= x;
+
+//          return ans;
+//     }
+// };
+
+
+//Approach 2 : using XOR
 class Solution {
 public:
-
     int missingNumber(vector<int>& nums) {
-
-        int ans =  (nums.size() * (nums.size() +1))/2 ;
-        for (auto x: nums)
-           ans -= x;
-
-         return ans;
+        int sum = nums.size();
+        for(int i = 0; i<nums.size(); i++) {
+            sum ^= i;
+            sum ^= nums[i];
+        }
+        return sum;
     }
-
-
-
 };
