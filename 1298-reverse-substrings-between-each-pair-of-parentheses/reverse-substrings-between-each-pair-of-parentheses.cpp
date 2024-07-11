2f02a2c4d37aad005460812ac298d1001ebffaf3
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-07-11 22:34:24 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (49.11%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,18 +1,57 @@
+
+//Approach 1: O(n^2) approach
+// class Solution {
+// public:
+//     string reverseParentheses(string s) {
+//         stack<int> st;
+//         string result;
+
+//         for (char &ch : s) {
+//             if (ch == '(') {
+//                 st.push(result.size());
+//             } else if (ch == ')') {
+//                 int len = st.top();
+//                 st.pop();
+//                 reverse(result.begin() + len, result.end());
+//             } else {
+//                 result.push_back(ch);
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+//Approach-2 (Linear Approach)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<int> st;
-        string result;
+        int n = s.length();
+        stack<int> openBracket;
+        vector<int> door(n);
 
-        for (char &ch : s) {
-            if (ch == '(') {
-                st.push(result.size());
-            } else if (ch == ')') {
-                int len = st.top();
-                st.pop();
-                reverse(result.begin() + len, result.end());
+        // First pass: Pair up parentheses
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                openBracket.push(i);
+            } else if (s[i] == ')') {
+                int j = openBracket.top();
+                openBracket.pop();
+                door[i] = j;
+                door[j] = i;
+            }
+        }
+
+        // Second pass: Build the result string
+        string result;
+        int direction = 1; //Left to Right
+        for (int i = 0; i < n; i += direction) {
+            if (s[i] == '(' || s[i] == ')') {
+                i = door[i];
+                direction = -direction;
             } else {
-                result.push_back(ch);
+                result += s[i];
             }
         }
         return result;
