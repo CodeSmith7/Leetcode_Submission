7464b4ca42fbe9cda00e1dced5455d7a099c1d20
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-08-01 12:31:37 +0530

    Time: 7 ms (50.40%) | Memory: 17.7 MB (16.71%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for (string &s : details) {
+            int one = s[12] - '0';
+            int two = s[11] - '0';
+
+            int age = two*10 + one;
+
+            if (age > 60) {
+                count++;
+            } 
+
+        }
+        return count;
+    }
+};
