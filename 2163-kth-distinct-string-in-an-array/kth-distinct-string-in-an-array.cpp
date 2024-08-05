37f2d7a9efa45d77ee92e06985c55c7da8b45195
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-08-05 10:52:17 +0530

    Time: 56 ms (7.29%) | Memory: 15.8 MB (98.41%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -1,30 +1,67 @@
+//Approach 1 : Brute force approach 
 class Solution {
 public:
+
+    int frquencyOfString(vector<string>& arr,string &s) {
+        int count = 0;
+        for (string &st : arr) {
+            if (st == s) {
+                count++;
+            }
+            if (count > 1) {
+                return count;
+            }
+        }
+        return count;
+    }
     string kthDistinct(vector<string>& arr, int k) {
-
-        unordered_map<string, int> mp;
-
-        for (string &st: arr) {
-            mp[st]++;
-        }
-
-        for (auto &[a,b] : mp) {
-            
-            cout << a << " -> " << b << endl;
-        }
-
-
-
-        for (string &s : arr) {
-            
-            if (mp[s] ==1 ) {
-                k--;
-            }
-
-            if (k == 0) {
-                return s;
+        int n = arr.size();
+        
+        int count = 0;
+        for(string &s : arr) {
+            if(frquencyOfString(arr, s) == 1) {
+                count++;
+                if(count == k) {
+                    return s;
+                }
             }
         }
+
         return "";
     }
 };
+
+
+
+//Approach 2 : using hash map
+// class Solution {
+// public:
+//     string kthDistinct(vector<string>& arr, int k) {
+
+//         unordered_map<string, int> mp;
+
+//         for (string &st: arr) {
+//             mp[st]++;
+//         }
+
+//         for (auto &[a,b] : mp) {
+            
+//             cout << a << " -> " << b << endl;
+//         }
+
+
+
+//         for (string &s : arr) {
+            
+//             if (mp[s] ==1 ) {
+//                 k--;
+//             }
+
+//             if (k == 0) {
+//                 return s;
+//             }
+//         }
+//         return "";
+//     }
+// };
+
